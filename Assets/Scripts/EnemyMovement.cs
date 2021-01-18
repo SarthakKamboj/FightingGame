8d07e491457034b60afc8bab3d81a275ecc9e2e1
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-01-18 03:47:10 -0800

    added enemy movement

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public Bounce bounce;
+    public float maxSpeed = 10f;
+    public Rigidbody rb;
+    Vector3 moveVec;
+
+    void Start() {
+        CalculateMoveVec();
+    }
+
+    void FixedUpdate() {
+        Debug.Log(rb.velocity);
+        if (rb.velocity.magnitude > maxSpeed) {
+            rb.AddForce(-moveVec * 0.1f, ForceMode.VelocityChange);
+        } else {
+            rb.AddForce(moveVec, ForceMode.VelocityChange);
+        }
+    }
+
+    void OnCollisionEnter(Collision col) {
+        Vector3 normalVec = col.contacts[0].normal;
+        if (col.collider.tag == "Ground") {
+            if(normalVec != Vector3.up) {
+                bounce.BounceGO(normalVec);
+                CalculateMoveVec();
+            }
+        } else {
+            CalculateMoveVec();
+        }
+    }
+
+    void CalculateMoveVec() {
+        float maxSpeedComp = maxSpeed / Mathf.Sqrt(2);
+        float xSpeed = Random.Range(-maxSpeedComp, maxSpeedComp);
+        float zSpeed = Random.Range(-maxSpeedComp, maxSpeedComp);
+        moveVec = new Vector3(xSpeed, 0f, zSpeed).normalized;
+    }
+    
+}
