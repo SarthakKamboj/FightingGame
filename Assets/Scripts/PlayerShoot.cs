8edf588da4554e8377d6ea27f5059f6c496f8902
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-01-18 02:57:59 -0800

    added player shooting ability

--- Assets/Scripts/PlayerShoot.cs
+++ Assets/Scripts/PlayerShoot.cs
@@ -4,29 +4,42 @@ public class PlayerShoot : MonoBehaviour
 {
     public LineRenderer shootRay;
     public float range = 10f;
+    public float sensitivity = 0.25f;
+    public float explosionImpact = 100f;
+    public float upwardsModifier = 10f;
+    float verticalMouseOffset = 0f;
+
+    void Update() {
+        verticalMouseOffset += Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+    }
 
     void LateUpdate() {
-
-        // Debug.Log(Input.mousePosition);
-        // Vector3 screenCenter = new Vector3(Screen.width/2, Screen.height/2, 0f);
-        // Ray ray = Camera.main.ScreenPointToRay(screenCenter);
-        // Debug.DrawRay(ray.origin, ray.direction * 10, Color.yellow);
-        Vector3 forwardVec = Quaternion.Euler(0f, Camera.main.transform.eulerAngles.y, 0f) * Vector3.forward;
-        Vector3[] positions = new Vector3[2] { transform.position, Vector3.zero };
-        // Vector3[] positions = new Vector3[2] { transform.position, (forwardVec.normalized * range) + transform.position };
-        shootRay.SetPositions(positions);
-        /*
-         if (Input.GetButtonDown("Fire1")) {
-            Shoot();
+        Vector3 aimVec = GetAimVec();
+        RenderLine(aimVec);
+        if (Input.GetButtonDown("Fire1")) {
+            Shoot(aimVec);
         }
-        */
+    }
+
+    Vector3 GetAimVec() {
+        Vector3 forwardVec = Quaternion.Euler(new Vector3(0f, Camera.main.transform.eulerAngles.y, 0f)) * Vector3.forward;
+        Vector3 rightVec = Vector3.Cross(Vector3.up, forwardVec);
+        return Quaternion.AngleAxis(verticalMouseOffset, rightVec) * forwardVec;
     }
     
-    void Shoot() {
+    void RenderLine(Vector3 aimVec) {
+        Vector3[] positions = new Vector3[2] { transform.position, transform.position + aimVec.normalized * range };
+        shootRay.SetPositions(positions);
+    }
+
+    void Shoot(Vector3 aimVec) {
         RaycastHit hit;
-        Vector3 forwardVec = Quaternion.Euler(0f, Camera.main.transform.eulerAngles.y, 0f) * Vector3.forward;
-        if (Physics.Raycast(transform.position, forwardVec.normalized, out hit, range)) {
-            Debug.Log("hit");
+        if (Physics.Raycast(transform.position, aimVec.normalized, out hit, range)) {
+            Collider collider = hit.collider.gameObject.GetComponent<Collider>(); 
+            Rigidbody goRb = hit.rigidbody;
+            if (collider && goRb) {
+                goRb.AddExplosionForce(explosionImpact, hit.point, collider.bounds.extents.x, upwardsModifier, ForceMode.VelocityChange);
+            }
         }
     }
 }
