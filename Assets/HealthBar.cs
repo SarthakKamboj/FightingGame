b44bd86110b886fef08581864e9d62527652919c
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-01-19 15:47:35 -0800

    added health bar script for hdu

---
+++ Assets/HealthBar.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    public Image healthSliderImage;
+    public Gradient gradient;
+    public Slider healthSlider;
+
+    public void UpdateHealthBar(float value) {
+        healthSlider.value = value;
+        healthSliderImage.color = gradient.Evaluate(value);
+    }
+
+}
