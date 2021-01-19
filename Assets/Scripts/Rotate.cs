84aaf6d2e78dc0626072d18e76c60614fb49b727
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-01-19 15:48:29 -0800

    added rotation to health power up

---
+++ Assets/Scripts/Rotate.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public float rotationsPerSecond = 2f;
+    Vector3 rotVec;
+
+    void Start() {
+        float degreesPerSecond = rotationsPerSecond * 360f;
+        rotVec = new Vector3(0f, degreesPerSecond, 0f);
+    }
+    void FixedUpdate() {
+        transform.rotation = Quaternion.Euler(rotVec * Time.fixedDeltaTime) * transform.rotation;
+    }
+}
