f38b2c6a19756db5d9dcba247eeb2f6a791cca3c
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-01-19 17:05:01 -0800

    added functionality to reduce score faster when using any powerup

---
+++ Assets/Scripts/HitPowerUp.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class HitPowerUp : MonoBehaviour
+{
+    public LayerMask layerMask;
+    public Score score;    
+
+    void OnTriggerEnter(Collider collider) {
+        if (IsPowerUp(collider.gameObject)) {
+            Debug.Log("power up hit");
+            score.scoreDecreaseMultiplier += 1;
+        }
+    }
+
+    bool IsPowerUp(GameObject obj) {
+        return (layerMask.value & (1 << obj.layer)) > 0;
+    }
+}
