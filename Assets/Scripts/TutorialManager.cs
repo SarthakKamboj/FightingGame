e97cdca73197941b7914731357243da814924a79
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-01-20 15:04:06 -0800

    added tutorial manager to iterate through tut sections

---
+++ Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class TutorialManager : MonoBehaviour
+{
+    List<Transform> tutorialSections = new List<Transform>();
+    int tutIdx = 0;
+
+    void Start() {
+        bool firstRound = true;
+        foreach (Transform tutSection in transform) {
+            if (firstRound) {
+                if (!tutSection.gameObject.activeSelf) {
+                    tutSection.gameObject.SetActive(true);
+                }
+                firstRound = false;
+            } else {
+                if (tutSection.gameObject.activeSelf) {
+                    tutSection.gameObject.SetActive(false);
+                }
+            }
+            tutorialSections.Add(tutSection);
+        }
+    }
+
+    public void MoveOnToNextTutSection() {
+        tutorialSections[tutIdx].gameObject.SetActive(false);
+        tutIdx += 1;
+        if (tutIdx < tutorialSections.Count) {
+            tutorialSections[tutIdx].gameObject.SetActive(true);
+        } else {
+            Debug.Log("finished tutorial");
+        }
+    }
+    
+}
