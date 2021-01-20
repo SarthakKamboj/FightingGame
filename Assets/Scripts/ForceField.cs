abf38b6538f7482de849f40f959ddd924003014b
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-01-19 21:58:31 -0800

    added script for force field and disabled collision with player & ground

---
+++ Assets/Scripts/ForceField.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class ForceField : MonoBehaviour
+{
+    
+    public float shieldBounceMult = 10f;
+    public GameObject player, ground;
+    public Collider forceFieldCollider;
+
+    void Start() {
+        DisableCol(player.GetComponent<Collider>());
+        foreach (Transform childTransform in ground.transform) {
+            DisableCol(childTransform.GetComponent<Collider>());
+        }
+        gameObject.SetActive(false);
+    }
+
+    void DisableCol(Collider otherCollider) {
+        Physics.IgnoreCollision(otherCollider, forceFieldCollider);
+    }
+    
+}
