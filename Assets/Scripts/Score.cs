7c26b20693e961d9fa7cb4ef8098f3735a3ba9a1
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-01-19 17:04:40 -0800

    added score hud and can now update its value

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class Score : MonoBehaviour
+{
+    public float score = 10000;
+    public TextMeshProUGUI scoreNumText;
+    [HideInInspector]
+    public int scoreDecreaseMultiplier = 1;
+    
+    void Update()
+    {
+        scoreNumText.text = ((int) score).ToString();
+        score -= Time.deltaTime * scoreDecreaseMultiplier;
+    }
+}
