2b684e3cc8efc508acf3d135a9cc8ff6a157bf6e
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-01-20 18:27:48 -0800

    added double jump tut section

---
+++ Assets/Scripts/DoubleClickedSpaceBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DoubleClickedSpaceBar : MonoBehaviour
+{
+    public TutorialManager tutorialManager;
+    public Rigidbody playerMeshRb;
+    int numTimesClickedSpace = 0;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            StartCoroutine(HandleJumpClick());
+        }
+
+        if (numTimesClickedSpace == 2) {
+            tutorialManager.MoveOnToNextTutSection();
+        }
+
+    }
+
+    IEnumerator HandleJumpClick() {
+        yield return new WaitForSeconds(Time.fixedDeltaTime * 2);
+        if (playerMeshRb.velocity.y != 0) {
+            numTimesClickedSpace += 1;
+        } else {
+            numTimesClickedSpace = 0;
+        }
+    }
+
+    
+}
