e732b548d85e2855bcd649a6365016c81068a695
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-01-20 18:27:32 -0800

    added jump tut section

---
+++ Assets/Scripts/ClickedSpaceBar.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class ClickedSpaceBar : MonoBehaviour
+{
+    public TutorialManager tutorialManager;
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            tutorialManager.MoveOnToNextTutSection();
+        }
+    }
+}
