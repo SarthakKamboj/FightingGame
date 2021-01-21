79e8eb295512e5f16d502f104e815377307c363d
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-01-20 18:28:30 -0800

    added powerup tut section

---
+++ Assets/Scripts/NotifyTutWhenPopwerUpHit.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class NotifyTutWhenPopwerUpHit : MonoBehaviour
+{
+    public TutorialManager tutorialManager;
+
+    void OnTriggerEnter(Collider collider) {
+        if (collider.tag == "Player") {
+            tutorialManager.MoveOnToNextTutSection();
+        }
+    }
+}
