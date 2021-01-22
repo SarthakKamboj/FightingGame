5ceddd784a86ff5529aa86a627fe1a7a74f1c0fd
SarthakKamboj <sarthakkamboj22@gmail.com>
2021-01-22 01:01:29 -0800

    added post processing

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            if (Time.timeScale == 0f) {
+                Time.timeScale = 1f;
+            } else {
+                Time.timeScale = 0f;
+            }
+        }
+    }
+}
